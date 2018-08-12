8fae5b00d30a7b72af0d1e79326f80f3bd9f387d
Ailim <ailim.moscoso@gmail.com>
2018-08-12 14:50:22 -0500

    add modal to restaurants

--- app.js
+++ app.js
@@ -31,19 +31,33 @@ options.addEventListener('change', function() {
         let div = document.createElement('div');
         div.className = 'result';
         let span = document.createElement('span');
-        let p = document.createElement('p');
         span.innerHTML = restaurant.name.bold();
+        span.className = 'restaurant';
         let image = document.createElement('img');
         image.src = restaurant.image;
-        image.className= 'foto';
+        image.className = 'foto';
         div.appendChild(span);
-        div.appendChild(p);
         div.appendChild(image);
         container.appendChild(div);
-        let a = document.createElement('a');
-        a.href = 
     });
-})
+    load(filters);
+});
+
+function load(filters) {
+    restaurants = document.getElementsByClassName('restaurant');
+    let dialog = document.getElementById('favDialog');
+    for (let i = 0; i < restaurants.length; i++) {
+        let restaurant = restaurants[i];
+        restaurant.addEventListener('click', function() {
+            dialog.removeChild(dialog.firstChild);
+            let p = document.createElement('p');
+            p.innerHTML = filters[i].name + '<br/>';
+            p.innerHTML += filters[i].address;
+            dialog.appendChild(p);
+            dialog.showModal();
+        });
+    }
+}
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
